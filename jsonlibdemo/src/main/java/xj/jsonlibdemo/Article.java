d81c12915242dcfb115beafae13d6cccd073a1fe
xujun <xj2@meitu.com>
2017-04-19 19:34:55 +0800

    修改类名

--- jsonlibdemo/src/main/java/xj/jsonlibdemo/Article.java
+++ jsonlibdemo/src/main/java/xj/jsonlibdemo/Article.java
@@ -3,12 +3,16 @@ package xj.jsonlibdemo;
 import com.example.Seriable;
 
 /**
- * @author meitu.xujun  on 2017/4/17 09:48
+ * 博客地址：http://blog.csdn.net/gdutxiaoxu
+ * @author  xujun  on 2017/4/19 19:01
  * @version 0.1
  */
+
+
 
 @Seriable
 public class Article {
     private String title;
     private String content;
+    private long time;
 }
